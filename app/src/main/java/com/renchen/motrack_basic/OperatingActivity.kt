e8e0ceaf95191784461e479169548946311c4038
renchen8394 <renchen8394@gmail.com>
2019-05-22 00:58:01 +0800

    Create @OperatingActivity.kt; Create @activity_operating.xml; Add 6 TextView in Activity; Use Intent to connect @StartActivity and @OperatingActivity;

---
+++ app/src/main/java/com/renchen/motrack_basic/OperatingActivity.kt
@@ -0,0 +1,12 @@
+package com.renchen.motrack_basic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OperatingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operating)
+    }
+}
--- app/src/main/java/com/renchen/motrack_basic/StartActivity.kt
+++ app/src/main/java/com/renchen/motrack_basic/StartActivity.kt
@@ -1,8 +1,11 @@
 package com.renchen.motrack_basic
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity(){
 
@@ -10,7 +13,10 @@ class StartActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-
+        nextActivity_button.setOnClickListener {
+            val operatingIntent = Intent(this, OperatingActivity::class.java)
+            startActivity(operatingIntent)
+        }
 
     }
 
