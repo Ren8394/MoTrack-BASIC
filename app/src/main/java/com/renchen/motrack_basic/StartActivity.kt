656379c6972f970db8c3449cd8df5388f9f13fc7
renchen8394 <renchen8394@gmail.com>
2019-05-21 21:14:18 +0800

    Delete @MainActivity and create @StartActivity

---
+++ app/src/main/java/com/renchen/motrack_basic/StartActivity.kt
@@ -0,0 +1,17 @@
+package com.renchen.motrack_basic
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+
+    }
+
+}
