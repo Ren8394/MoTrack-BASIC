68473df08d7056fa37fe74c9237869b00f43fef1
renchen8394 <renchen8394@gmail.com>
2019-05-22 17:29:56 +0800

    Add sensor function(fail); Different layout;

--- app/src/main/java/com/renchen/motrack_basic/OperatingActivity.kt
+++ app/src/main/java/com/renchen/motrack_basic/OperatingActivity.kt
@@ -1,12 +1,49 @@
 package com.renchen.motrack_basic
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_operating.*
+import java.util.logging.Logger
 
-class OperatingActivity : AppCompatActivity() {
+class OperatingActivity: Activity(), SensorEventListener {
+
+    private lateinit var mSensorManager: SensorManager
+    private var mAccelerometer : Sensor ?= null
+    private var mMagnetometer : Sensor ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operating)
+
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val sensorValues =
+            "X-axis : " + event.values[0] + "\t" +
+            "Y-axis : " + event.values[1] + "\t" +
+            "Z-axis : " + event.values[2]
+
+        if (event.sensor == mAccelerometer) {
+            Acc_text.text = sensorValues
+        }
+        if (event.sensor == mMagnetometer) {
+            Mg_text.text = sensorValues
+        }
     }
 }
--- app/src/main/java/com/renchen/motrack_basic/StartActivity.kt
+++ app/src/main/java/com/renchen/motrack_basic/StartActivity.kt
@@ -1,13 +1,11 @@
 package com.renchen.motrack_basic
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_start.*
 
-class StartActivity : AppCompatActivity(){
+class StartActivity : Activity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
