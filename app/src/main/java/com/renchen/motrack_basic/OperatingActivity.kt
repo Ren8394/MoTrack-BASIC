0ecdc4cc5f500a31607a9272268a7c34331e4b4e
renchen8394 <renchen8394@gmail.com>
2019-06-10 09:45:56 +0800

    Applied Accelerometer and Magnetometer

--- app/src/main/java/com/renchen/motrack_basic/OperatingActivity.kt
+++ app/src/main/java/com/renchen/motrack_basic/OperatingActivity.kt
@@ -10,18 +10,31 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_operating.*
 
+/*
+This class is an activity while operating position tracking.
+
+1. the magnetometer and accelerometer
+*/
+
 class OperatingActivity: Activity(), SensorEventListener {
 
     private lateinit var mSensorManager: SensorManager
-    private var mAccelerometer : Sensor ?= null
+    private val accelerometerReading = FloatArray(3)    // Use to catch the x, y, and z acceleration value
+    private val magnetometerReading = FloatArray(3)     // Use to catch the x, y, and z magnetic filed value
+
+    private val rotationMatrix = FloatArray(9)          // Rotation matrix based on current readings from accelerometer and magnetometer
+    private val orientationAngles = FloatArray(3)       /* Express the updated rotation matrix as three orientation angles
+                                                                Azimuth(0) -> degrees of rotation about -z axis
+                                                                              (0-N, 90-E, 180-S, 270-W)
+                                                                Pitch(1)   -> degrees of rotation about x axis
+                                                                Roll(2)    -> degrees of rotation about y axis
+                                                             */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operating)
 
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
     }
 
     private fun requestPermission() {
@@ -31,38 +44,51 @@ class OperatingActivity: Activity(), SensorEventListener {
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        val sensorValues =
-            "X-axis : " + event.values[0] + "\t" +
-            "Y-axis : " + event.values[1] + "\t" +
-            "Z-axis : " + event.values[2]
-
-        if (event.sensor == mAccelerometer) {
-            Acc_text.text = sensorValues
-            positionTrack(event.values)
+        // Get readings from accelerometer and magnetometer.
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+            Acc_text.text = "WELL DONE"
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+            Mg_text.text = "SUCCESSED"
         }
 
+        // Update device's orientation
+        updateOrientationAngles()
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    private fun updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
     }
 
     override fun onResume() {
         super.onResume()
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            accelerometer -> mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also {
+            magneticField -> mSensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL)
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        mSensorManager.unregisterListener(this, mAccelerometer)
+
+        // Don't receive any more updates from either sensor.
+        mSensorManager.unregisterListener(this)
     }
 
-    private fun positionTrack(value : FloatArray) {
-
-        if (value[2] >= 10.5) {
-            Acc_text.text = "GO GO GO"
-        }
-
-    }
 
 }
